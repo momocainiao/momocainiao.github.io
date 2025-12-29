590470044799c6ed382133bc5f8eb37a71cd9648
momocainiao <cainiao314@gmail.com>
2025-12-29 21:49:02 +0800

    upgrade

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,14 @@
 import { defineConfig } from 'astro/config';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 export default defineConfig({
   site: 'https://momocainiao.github.io',
-  base: '/'
+  base: '/',
+    markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [
+      [rehypeKatex, { output: 'mathml' }] // 强制只输出 HTML，不输出 MathML
+    ],
+  },
 });
